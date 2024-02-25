937cf06a97c0af356df1ea80eabdea889ab1b728
Junn Eric Timoteo <juntyms@yahoo.com>
2024-02-25 20:10:34 +0800

    updated routes

--- CapstoneMedSync/routes/web.php
+++ CapstoneMedSync/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,40 +18,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\SessionsController;
-            
+//Route::get('/', function () {return redirect('sign-in');})->middleware('guest');
 
-Route::get('/', function () {return redirect('sign-in');})->middleware('guest');
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
-Route::get('sign-up', [RegisterController::class, 'create'])->middleware('guest')->name('register');
-Route::post('sign-up', [RegisterController::class, 'store'])->middleware('guest');
-Route::get('sign-in', [SessionsController::class, 'create'])->middleware('guest')->name('login');
-Route::post('sign-in', [SessionsController::class, 'store'])->middleware('guest');
-Route::post('verify', [SessionsController::class, 'show'])->middleware('guest');
-Route::post('reset-password', [SessionsController::class, 'update'])->middleware('guest')->name('password.update');
+Route::get('sign-up', [RegisterController::class, 'create'])->name('register');
+Route::post('sign-up', [RegisterController::class, 'store']);
+Route::get('sign-in', [SessionsController::class, 'create'])->name('login');
+Route::post('sign-in', [SessionsController::class, 'store']);
+Route::post('verify', [SessionsController::class, 'show']);
+Route::post('reset-password', [SessionsController::class, 'update'])->name('password.update');
 
 Route::get('verify', function () {
-	return view('sessions.password.verify');
-})->middleware('guest')->name('verify'); 
-Route::get('/reset-password/{token}', function ($token) {
-	return view('sessions.password.reset', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
+        return view('sessions.password.verify');
+    })->middleware('guest')->name('verify');
 
-Route::post('sign-out', [SessionsController::class, 'destroy'])->middleware('auth')->name('logout');
-Route::get('profile', [ProfileController::class, 'create'])->middleware('auth')->name('profile');
-Route::post('user-profile', [ProfileController::class, 'update'])->middleware('auth');
+Route::get('/reset-password/{token}', function ($token) {
+        return view('sessions.password.reset', ['token' => $token]);
+    })->middleware('guest')->name('password.reset');
+
+
+
+/** All Authenticated Routes will be here */
 Route::group(['middleware' => 'auth'], function () {
-	Route::get('tables', function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+
+    Route::get('/tables', function () {
 		return view('pages.tables');
 	})->name('tables');
-	Route::get('user-management', function () {
+
+    Route::get('/user-management', function () {
 		return view('pages.laravel-examples.user-management');
 	})->name('user-management');
+
+    Route::get('/profile', [ProfileController::class, 'create'])->name('profile');
+    Route::post('/profile', [ProfileController::class, 'update'])->name('showprofile');
+
+    Route::post('sign-out', [SessionsController::class, 'destroy'])->name('logout');
+
 });
