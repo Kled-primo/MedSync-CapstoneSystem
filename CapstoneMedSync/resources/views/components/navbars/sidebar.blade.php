8cd6f3d8c9d02ef73d817da6b5d1287ae98d2a40
Kled-primo <158154173+Kled-primo@users.noreply.github.com>
2024-03-06 02:42:04 +0800

    Side NavBar changes

--- CapstoneMedSync/resources/views/components/navbars/sidebar.blade.php
+++ CapstoneMedSync/resources/views/components/navbars/sidebar.blade.php
@@ -13,7 +13,6 @@
         </a>
     </div>
     <hr class="horizontal dark mt-0 mb-3">
-    <div class="collapse navbar-collapse  w-auto  max-height-vh-20" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item mt-3">
                 <li class="nav-item">
@@ -26,15 +25,32 @@
                     </a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'user-management' ? ' active bg-gradient-primary' : '' }} "
+                        href="{{ route('user-management') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Inventory</span>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link text-white {{ $activePage == 'tables' ? ' active bg-gradient-primary' : '' }} "
                         href="{{ route('tables') }}">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="fas fa-lg ps-1 pe-2 text-center material-icons opacity-10">table_view</i>
                         </div>
-                        <span class="nav-link-text ms-1">Tables</span>
+                        <span class="nav-link-text ms-1">Point Of Sale</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'user-management' ? ' active bg-gradient-primary' : '' }} "
+                        href="{{ route('user-management') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Transaction Records</span>
                     </a>
                 </li>
             </li>
         </ul>
-    </div>
 </aside>
